c40f4ccd6a76b2a08887095b65eee46fe0f815dd
Omar <omar.garcia@morsa.com.mx>
2025-12-09 17:44:01 -0600

    Estilos a CANTIDADES

--- src/components/ControlPresupuesto.js
+++ src/components/ControlPresupuesto.js
@@ -13,25 +13,23 @@ const ControlPresupuesto = ({ presupuesto}) => {
         />
       </View>
 
-      <View>
-        <Text>
-          <Text>Presupuesto: </Text>
+      <View style={styles.contenedorTexto}>
+        <Text style={styles.valor}>
+          <Text style={styles.label}>Presupuesto: </Text>
 
           {/* {presupuesto} */}
           {formatearCantidad(presupuesto)}
         </Text>
 
-        <Text>
-          <Text>Disponible: </Text>
+        <Text style={styles.valor}>
+          <Text style={styles.label}>Disponible: </Text>
 
-          {/* {presupuesto} */}
           {formatearCantidad(presupuesto)}
         </Text>
 
-        <Text>
-          <Text>Gastado: </Text>
+        <Text style={styles.valor}>
+          <Text style={styles.label}>Gastado: </Text>
 
-          {/* {presupuesto} */}
           {formatearCantidad(presupuesto)}
         </Text>
       </View>
@@ -69,6 +67,21 @@ const styles = StyleSheet.create({
   imagen: {
     width: 250,
     height: 250
+  },
+
+  contenedorTexto: {
+    marginTop: 50
+  },
+
+  valor: {
+    fontSize: 24,
+    textAlign: 'center',
+    marginBottom: 10
+  },
+
+  label: {
+    fontWeight: '700',
+    color: '#3B82F6'
   }
 });
 
