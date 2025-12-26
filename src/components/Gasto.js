a8d19e82425ee110a03c94ae844fb74e3bf0044e
Omar <omar.garcia@morsa.com.mx>
2025-12-26 14:08:08 -0600

    Mostrando IMG de la CATEGORIA de GASTO

--- src/components/Gasto.js
+++ src/components/Gasto.js
@@ -1,13 +1,37 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import globalStyles from '../styles';
+import { formatearCantidad } from '../helpers';
+
+const diccionarioIconos = {
+  ahorro: require('../img/icono_ahorro.png'),
+  comida: require('../img/icono_comida.png'),
+  casa: require('../img/icono_casa.png'),
+  gastos: require('../img/icono_gastos.png'),
+  ocio: require('../img/icono_ocio.png'),
+  salud: require('../img/icono_salud.png'),
+  suscripciones: require('../img/icono_suscripciones.png'),
+};
 
 const Gasto = ({ gasto }) => {
   const { nombre, categoria, cantidad, id } = gasto;
 
   return (
     <View style={styles.contenedor}>
-      <Text style={styles.nombre}>{nombre}</Text>
+
+      <View>
+
+        <Image
+          source={diccionarioIconos[categoria]}
+        />
+
+        <View>
+          <Text>{categoria}</Text>
+          <Text>{nombre}</Text>
+        </View>
+
+        <Text>{formatearCantidad(cantidad)}</Text>
+      </View>
     </View>
   );
 };
@@ -15,11 +39,7 @@ const Gasto = ({ gasto }) => {
 const styles = StyleSheet.create({
   contenedor: {
     ...globalStyles.contenedor,
-  },
-
-  nombre: {
-    fontSize: 18,
-    fontWeight: 'bold',
+    marginBottom: 20,
   },
 });
 
