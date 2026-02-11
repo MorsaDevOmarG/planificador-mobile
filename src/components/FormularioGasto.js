b123bb04f8999210f11587e52da1eff7fc87d342
Omar <omar.garcia@morsa.com.mx>
2026-02-11 10:59:01 -0600

    Mostrando el BTN de ELIMINAR de forma condicional

--- src/components/FormularioGasto.js
+++ src/components/FormularioGasto.js
@@ -48,14 +48,25 @@ const FormularioGasto = ({
           <Text style={styles.btnTexto}>Cancelar</Text>
         </Pressable>
 
-        <Pressable
+        {/* <Pressable
           style={[styles.btn, styles.btnEliminar]}
           onLongPress={() => {
             eliminarGasto(id);
           }}
         >
           <Text style={styles.btnTexto}>Eliminar</Text>
-        </Pressable>
+        </Pressable> */}
+
+        {!!id && (
+          <Pressable
+            style={[styles.btn, styles.btnEliminar]}
+            onLongPress={() => {
+              eliminarGasto(id);
+            }}
+          >
+            <Text style={styles.btnTexto}>Eliminar</Text>
+          </Pressable>
+        )}
       </View>
 
       <View style={styles.formulario}>
