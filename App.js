b536be71b29eeb064ce52cffce4c3f23180b70f1
Omar <omar.garcia@morsa.com.mx>
2026-02-13 13:26:11 -0600

    OBTENER gastos de ASYNC STORAGE

--- App.js
+++ App.js
@@ -67,6 +67,20 @@ const App = () => {
   }, [isValidPresupuesto]);
 
   useEffect(() => {
+    const obtenerGastosStorage = async () => {
+      try {
+        const gastosStorage = await AsyncStorage.getItem('planificador_gastos');
+
+        setGastos(gastosStorage ? JSON.parse(gastosStorage) : []);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    obtenerGastosStorage();
+  }, []);
+
+  useEffect(() => {
     const guardarGastosStorage = async () => {
       try {
         await AsyncStorage.setItem(
